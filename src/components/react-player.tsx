ff446a669707e31364792ad5d9398bc7d97cf31c
kelson8 <kelson@kelsoncraft.net>
2026-06-13 01:32:27 -0400

    Make video pages get url from the navbar
    * Now the video pages get the url from the path set in the browser instead of being hard-coded.
    
    * Make video player display invalid url if the video doesn't exist.
    * Add blank public\videos-web folder.
    * Make axios work for checking errors on the video pages.

--- src/components/react-player.tsx
+++ src/components/react-player.tsx
@@ -1,10 +1,12 @@
 // import { Plyr, usePlyr } from 'plyr-react';
-import { Plyr } from 'plyr-react';
+import {Plyr} from 'plyr-react';
 // import 'plyr-react/dist/plyr.css';
 import "plyr-react/plyr.css"
 // import { useRef } from 'react';
 
-import type { PlyrProps } from 'plyr-react';
+import type {PlyrProps} from 'plyr-react';
+import React from "react";
+import axios from "axios";
 
 // https://github.com/chintan9/plyr-react
 // Taken from KCNet-Vite
@@ -30,21 +32,56 @@ const defaultVolume: number = 0.2;
  * @returns The mime type of the video.
  */
 function mimeTypeFromUrl(url: string) {
+    if (!url) {
+        return;
+    }
     const ext = url.split('?')[0].split('.').pop()?.toLowerCase() ?? '';
     switch (ext) {
-        case 'mp4': return 'video/mp4';
-        case 'webm': return 'video/webm';
+        case 'mp4':
+            return 'video/mp4';
+        case 'webm':
+            return 'video/webm';
         case 'ogg':
-        case 'ogv': return 'video/ogg';
-        default: return 'video/mp4';
+        case 'ogv':
+            return 'video/ogg';
+        default:
+            return 'video/mp4';
     }
 }
 
 // Player source configuration
 // This runs Plyr with a custom video url.
-export function VideoPlayer({ videoUrl, poster, plyrControls, volume }: Props) {
+export function VideoPlayer({videoUrl, poster, plyrControls, volume}: Props) {
 
     const type = mimeTypeFromUrl(videoUrl);
+
+    // Switching to React useState seems to have fixed this.
+    const [videoUrlValid, setVideoUrlValid] = React.useState(false);
+
+
+    // Check if the path exists on the url
+    // Now this displays an error if the video doesn't exist.
+    // TODO Fix the slight delay on the error code, it will run breifly even if the video is valid.
+    const response = axios.get(
+        videoUrl,
+        {
+            method: 'GET'
+        }
+    )
+        .then(res => {
+            setVideoUrlValid(true);
+            // console.log("Video url was valid");
+        })
+        .catch(err => {
+            setVideoUrlValid(false);
+            // console.log(err);
+        });
+
+    // This works now! It has a slight delay and does display for like a second or two when the video exists.
+    if (!videoUrlValid) {
+        return <p>The video could not be played! Is the url correct?</p>
+    }
+
     const plyrProps: PlyrProps = {
         source: {
             type: "video",
@@ -52,6 +89,7 @@ export function VideoPlayer({ videoUrl, poster, plyrControls, volume }: Props) {
                 {
                     src: videoUrl,
                     type: type,
+                    // TODO Make this get the size from the video file, such as 720p, 1080p or other formats.
                     size: 720,
                 },
             ],
